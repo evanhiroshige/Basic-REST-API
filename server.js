de1bde45c10b23c9898def4b658d76a95ee1a65e
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-10-18 19:55:11 -0400

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
-
+var dbURI = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
@@ -9,22 +12,23 @@ app.use(bodyparser.json());
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-require('./app/routes/routes.js')(app);
+
 
 app.get('/api', (req, res) => {
     res.json({"message": "Basic Api"})
 });
 
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, {
+console.log(dbURI);
+
+mongoose.connect(dbURI, {
     useNewUrlParser: true
 }).then(() => {
     console.log('Connected to database.');
 }).catch(err => {
     console.log('Unable to connect to database.');
 });
-
-
+require('./app/routes/routes.js')(app);
+console.log(process.env.PORT);
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started.")
 });
