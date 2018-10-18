66747d9d5345c4b3c8df9d0c53ef1f4777b86e63
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-10-18 18:48:22 -0400

    heroku support

--- config/database.config.js
+++ config/database.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-    local : {
-        url: 'mongodb://localhost:27017/basic-rest',
-        port : 3000
-    },
-    heroku : {
-        url : process.env.MONGODB_URI,
-        port : process.env.PORT
-    }
+    url : process.env.MONGODB_URI,
+    port : process.env.PORT
 };
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import * as dbConfig from "./config/database.config";
-
 const express = require('express');
 const bodyparser = require('body-parser');
 
@@ -9,23 +7,23 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 app.get('/api', (req, res) => {
     res.json({"message": "Basic Api"})
 });
 
-require('./app/routes/routes.js')(app);
-
-const db = dbConfig.heroku;
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect(db.url, {
+mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
     console.log('Connected to database.');
 }).catch(err => {
     console.log('Unable to connect to database.');
 });
+
+require('./app/routes/routes.js')(app);
 
 app.listen(db.port, () => {
     console.log("Server started.")
