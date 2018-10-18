f2f07e57afe56d6cf1560d4704c9ab7a90a483de
hiroshige.e <hiroshige.e@husky.neu.edu>
2018-10-18 18:32:52 -0400

    heroku support

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/objects'
-}
+    local : {
+        url: 'mongodb://localhost:27017/basic-rest',
+        port : 3000
+    },
+    heroku : {
+        url : process.env.MONGODB_URI,
+        port : process.env.PORT
+    }
+};
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import * as dbConfig from "./config/database.config";
+
 const express = require('express');
 const bodyparser = require('body-parser');
 
@@ -5,23 +7,19 @@ const app = express();
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
+
+
 app.get('/api', (req, res) => {
     res.json({"message": "Basic Api"})
 });
 
-
 require('./app/routes/routes.js')(app);
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000")
-});
-
-const dbConfig = require('./config/database.config.js');
+const db = dbConfig.heroku;
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
+mongoose.connect(db.url, {
     useNewUrlParser: true
 }).then(() => {
     console.log('Connected to database.');
@@ -29,39 +27,6 @@ mongoose.connect(dbConfig.url, {
     console.log('Unable to connect to database.');
 });
 
-// server.js
-
-// BASE SETUP
-// =============================================================================
-//
-// // call the packages we need
-// var express    = require('express');        // call express
-// var app        = express();                 // define our app using express
-// var bodyParser = require('body-parser');
-//
-// // configure app to use bodyParser()
-// // this will let us get the data from a POST
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-//
-// var port = process.env.PORT || 8080;        // set our port
-//
-// // ROUTES FOR OUR API
-// // =============================================================================
-// var router = express.Router();              // get an instance of the express Router
-//
-// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function(req, res) {
-//     res.json({ message: 'hooray! welcome to our api!' });
-// });
-//
-// // more routes for our API will happen here
-//
-// // REGISTER OUR ROUTES -------------------------------
-// // all of our routes will be prefixed with /api
-// app.use('/api', router);
-//
-// // START THE SERVER
-// // =============================================================================
-// app.listen(port);
-// console.log('Magic happens on port ' + port);
+app.listen(db.port, () => {
+    console.log("Server started.")
+});
